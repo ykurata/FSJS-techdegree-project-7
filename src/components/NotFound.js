5879eb05826d1209e66e0c4af3af734e5a85d325
ykuruta <yasukokurata@gmail.com>
2019-01-31 23:31:12 -0500

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div>
-      <h2>Page Not Found</h2>
-      <button><a href='/'>Home</a></button>
+      <h2>404 Page Not Found</h2>
+      <h2><a href='/'>Home</a></h2>
     </div>
   );
 }

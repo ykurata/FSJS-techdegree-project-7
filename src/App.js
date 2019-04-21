350a70b40884d86b3817f3923bb07ca852785ad7
ykuruta <yasukokurata@gmail.com>
2019-04-21 19:07:34 -0400

    updated App.js. deleted cat, dog, flower components

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ import axios from 'axios';
 import Form from './components/Form';
 import Nav from './components/Nav';
 import Results from './components/Results';
-import Cats from './components/Cats';
-import Dogs from './components/Dogs';
-import Flowers from './components/Flowers';
+import NotFound from './components/NotFound';
 
 // flickr api key
 import apiKey from './config';
@@ -23,16 +21,37 @@ class App extends Component {
     super();
     this.state = {
       photos: [],
+      catsPhotos: [],
+      dogsPhotos: [],
+      flowersPhotos: [],
       loading: true,
       searchText: ''
     };
   }
+
   componentDidMount() {
     this.performSearch();
+    this.getPhotos("cats", "catsPhotos");
+    this.getPhotos("dogs", "dogsPhotos");
+    this.getPhotos("flowers", "flowersPhotos");
   }
 
+  getPhotos = (query, photos) => {
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({
+          [photos]: response.data.photos.photo,
+          loading: false,
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
+
   // Search photos from api
-  performSearch = (query = "cats") => {
+  performSearch = (query) => {
       axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
         .then(response => {
           this.setState({
@@ -46,6 +65,7 @@ class App extends Component {
         });
   }
 
+
   // render each components
   render() {
     console.log(this.state.photos);
@@ -55,11 +75,12 @@ class App extends Component {
           <Form onSearch={this.performSearch} />
           <Nav />
             <Switch>
-              <Route exact path="/" component={Cats} />
+              <Route exact path="/" render={ () => <Results data={this.state.catsPhotos}  />} />
               <Route path={`/search/${this.state.searchText}`} render={ () => <Results data={this.state.photos} text={this.state.searchText} />} />
-              <Route path="/cats" component={Cats} />
-              <Route path="/dogs" component={Dogs} />
-              <Route path="/flowers" component={Flowers} />
+              <Route path="/cats" render={ () => <Results data={this.state.catsPhotos}  />} />
+              <Route path="/dogs" render={ () => <Results data={this.state.dogsPhotos}  />} />
+              <Route path="/flowers" render={ () => <Results data={this.state.flowersPhotos}  />} />
+              <Route component={NotFound} />
             </Switch>
         </div>
       </BrowserRouter>
